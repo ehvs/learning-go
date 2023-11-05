b8d7a7b7f63545290d968a17a33a4bfefb636481
ehvs <hevs159@outlook.com>
2023-11-05 19:06:31 +0100

    challenge + 1 extra challenge, missing the 2nd

--- Udemy-Go-with-games/games-01_hangman/main.go
+++ Udemy-Go-with-games/games-01_hangman/main.go
@@ -10,31 +10,40 @@ import (
 	"unicode"
 )
 
+// Declare a variable globaly outside main function
+
 var inputReader = bufio.NewReader(os.Stdin)
+
 var dictionary = []string{
 	"Zombie",
 	"Gopher",
-	"Brasil",
+	"United States of America",
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	targetWord := getRandomWord()
-	//targetWord = "United States of America"
-	fmt.Println(targetWord)
-	guessedLetters := bagOfLetters(targetWord)
+	guessedLetters := initializedGuess(targetWord)
 	hangmanState := 0
+
+	fmt.Println(targetWord)
 
 	for !isGameOver(targetWord, guessedLetters, hangmanState) {
 		printGameState(targetWord, guessedLetters, hangmanState)
 		input := readInput()
 		if len(input) != 1 {
-			fmt.Println("Invalid input. Please use letters only...")
-			continue // keep the game as it is
+			fmt.Println("Invalid input. Too long input]")
+			continue // to keep the game state as it is. Do not reset anything.
 		}
 
-		letter := rune(input[0]) // converting to rune
-		if validateGuess(targetWord, letter) {
+		letter := rune(input[0])
+		//fmt.Printf("\n ...You inserted: %c \n", letter)
+		for l := range guessedLetters {
+			if l == letter {
+				fmt.Printf("Letter %c, already used \n", l)
+			}
+		}
+		if isCorrectGuess(targetWord, letter) {
 			guessedLetters[letter] = true
 		} else {
 			hangmanState++
@@ -50,91 +59,56 @@ func main() {
 	}
 }
 
-// Derive a word we have to guess
+// Function structure - nome (entrada) saida { codigo }
 func getRandomWord() string {
-
 	targetWord := dictionary[rand.Intn(len(dictionary))]
 	return targetWord
 }
 
-// Read Input
-func readInput() string {
-	fmt.Print("> ")
-	input, err := inputReader.ReadString('\n')
-	if err != nil {
-		panic(err)
-	}
-
-	return strings.TrimSpace(input)
-}
-
-// Validating guessed letter
-func validateGuess(targetWord string, letter rune) bool {
-	return strings.ContainsRune(targetWord, letter)
-
-}
-
-func bagOfLetters(targetWord string) map[rune]bool {
+func initializedGuess(targetWord string) map[rune]bool {
 	guessedLetters := map[rune]bool{}
-	guessedLetters[unicode.ToLower(rune(targetWord[0]))] = true                 // convert targetWord to a rune
-	guessedLetters[unicode.ToLower(rune(targetWord[len(targetWord)-1]))] = true // get the last letter
-	//guessedLetters[rune('s')] = true
+	guessedLetters[unicode.ToLower(rune(targetWord[0]))] = true
+	guessedLetters[unicode.ToLower(rune(targetWord[len(targetWord)-1]))] = true
 
 	return guessedLetters
 }
 
-// Check if word is guessed
+func isGameOver(targetWord string, guessedLetters map[rune]bool, hangmanState int) bool {
+	return isWordGuessed(targetWord, guessedLetters) || isHangmanComplete(hangmanState)
+}
+
+// Em um loop, para cada caractere da palavra em 'targetWord', se esse caractere nao existe no map 'guessedLetters'
 func isWordGuessed(targetWord string, guessedLetters map[rune]bool) bool {
-	for _, ch := range targetWord {
-		if !guessedLetters[unicode.ToLower(ch)] {
+	for _, character := range targetWord {
+		if !guessedLetters[unicode.ToLower(character)] {
 			return false
 		}
 	}
 	return true
 }
 
-// Check if Hangman is complete
 func isHangmanComplete(hangmanState int) bool {
 	return hangmanState >= 9
-
 }
 
-// Game Over
-func isGameOver(
-	targetWord string,
-	guessedLetters map[rune]bool,
-	hangmanState int,
-) bool {
-	return isWordGuessed(targetWord, guessedLetters) ||
-		isHangmanComplete(hangmanState)
-}
-
-func printGameState(
-	targetWord string,
-	guessedLetters map[rune]bool,
-	hangmanState int,
-) {
+// RUNE eh uma letra/caractere
+func printGameState(targetWord string, guessedLetters map[rune]bool, hangmanState int) {
 	fmt.Println(getWordGuessingProgress(targetWord, guessedLetters))
 	fmt.Println(getHangmanDrawing(hangmanState))
-
 }
 
-func getWordGuessingProgress(
-	targetWord string,
-	guessedLetters map[rune]bool,
-) string {
+func getWordGuessingProgress(targetWord string, guessedLetters map[rune]bool) string {
 	result := ""
-	for _, ch := range targetWord { // get the index (i) and the character (ch) inside the string. Bcoz wont need the index (i), replaces to (_)
-		if ch == ' ' {
+	for _, character := range targetWord {
+		if character == ' ' {
 			result += " "
-		} else if guessedLetters[unicode.ToLower(ch)] == true {
-			result += fmt.Sprintf("%c", ch) // Printf reads formatation e.g. %c $.3f
+		} else if guessedLetters[unicode.ToLower(character)] == true {
+			result += fmt.Sprintf("%c", character) //Printf to format  || Sprintf format anrd returns as string
 		} else {
 			result += "_"
 		}
 		result += " "
 	}
-
 	return result
 }
 
@@ -143,6 +117,26 @@ func getHangmanDrawing(hangmanState int) string {
 	if err != nil {
 		panic(err)
 	}
-
 	return (string(data))
 }
+
+func readInput() string {
+	fmt.Printf("> ")
+	// r := bufio.NewReader(os.Stdin) -- instead, declare globaly for recycling purposes
+	input, err := inputReader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSpace(input)
+}
+
+func isCorrectGuess(targetWord string, letter rune) bool {
+	// checking if a letter is inside a word 'containsRune'
+	return strings.ContainsRune(targetWord, letter)
+}
+
+/* Extra challenge
+- if a letter was already guessed, print "letter already used" -- DONE
+- implement 'hint', print a letter that was not used yet
+  - limit the user to one hint only
+*/
