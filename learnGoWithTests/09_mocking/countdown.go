b378667df533ba88a7a78d78a6214f792a6c0bb7
Ehvs <hevs159@outlook.com>
2024-08-17 19:09:48 +0200

    finished 09

--- learnGoWithTests/09_mocking/countdown.go
+++ learnGoWithTests/09_mocking/countdown.go
@@ -31,13 +31,25 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-type DefaultSleeper struct{}
+// Accepts what we need for configuration and testing.
+// using duration to configure the time slept and sleep as a way to pass in a sleep function.
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+type SpyTime struct {
+	durationSlept time.Duration
+}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
--- learnGoWithTests/09_mocking/countdown_test.go
+++ learnGoWithTests/09_mocking/countdown_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
@@ -22,5 +23,16 @@ Go!`
 	}
 	if spySleeper.Calls != 3 {
 		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+	}
+}
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
 }
